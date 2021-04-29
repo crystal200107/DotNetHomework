6a1418f07e49b503e9e7eedb877353c0ba3cd7d8
crystal200107 <79796600+crystal200107@users.noreply.github.com>
2021-04-29 13:30:28 +0800

    Add files via upload

---
+++ Homework7/7.0/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace _7._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBoxLdegree = new System.Windows.Forms.TextBox();
+            this.textBoxRdegree = new System.Windows.Forms.TextBox();
+            this.textBoxLLrate = new System.Windows.Forms.TextBox();
+            this.textBoxRLrate = new System.Windows.Forms.TextBox();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(555, 565);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(555, 474);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(320, 91);
+            this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(103, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "绘制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "递归深度：";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "主干长度：";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "右分支长度比：";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "左分支长度比：";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(555, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(320, 474);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxLdegree);
+            this.tabPage1.Controls.Add(this.textBoxRdegree);
+            this.tabPage1.Controls.Add(this.textBoxLLrate);
+            this.tabPage1.Controls.Add(this.textBoxRLrate);
+            this.tabPage1.Controls.Add(this.textBoxLength);
+            this.tabPage1.Controls.Add(this.textBoxN);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(312, 441);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "绘图参数";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLdegree
+            // 
+            this.textBoxLdegree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxLdegree.Location = new System.Drawing.Point(141, 326);
+            this.textBoxLdegree.Name = "textBoxLdegree";
+            this.textBoxLdegree.Size = new System.Drawing.Size(125, 27);
+            this.textBoxLdegree.TabIndex = 11;
+            this.textBoxLdegree.TextChanged += new System.EventHandler(this.textBoxLdegree_TextChanged);
+            // 
+            // textBoxRdegree
+            // 
+            this.textBoxRdegree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxRdegree.Location = new System.Drawing.Point(141, 272);
+            this.textBoxRdegree.Name = "textBoxRdegree";
+            this.textBoxRdegree.Size = new System.Drawing.Size(125, 27);
+            this.textBoxRdegree.TabIndex = 10;
+            this.textBoxRdegree.TextChanged += new System.EventHandler(this.textBoxRdegree_TextChanged);
+            // 
+            // textBoxLLrate
+            // 
+            this.textBoxLLrate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxLLrate.Location = new System.Drawing.Point(141, 215);
+            this.textBoxLLrate.Name = "textBoxLLrate";
+            this.textBoxLLrate.Size = new System.Drawing.Size(125, 27);
+            this.textBoxLLrate.TabIndex = 9;
+            this.textBoxLLrate.TextChanged += new System.EventHandler(this.textBoxLLrate_TextChanged);
+            // 
+            // textBoxRLrate
+            // 
+            this.textBoxRLrate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxRLrate.Location = new System.Drawing.Point(141, 161);
+            this.textBoxRLrate.Name = "textBoxRLrate";
+            this.textBoxRLrate.Size = new System.Drawing.Size(125, 27);
+            this.textBoxRLrate.TabIndex = 8;
+            this.textBoxRLrate.TextChanged += new System.EventHandler(this.textBoxRLrate_TextChanged);
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxLength.Location = new System.Drawing.Point(141, 103);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(125, 27);
+            this.textBoxLength.TabIndex = 7;
+            this.textBoxLength.TextChanged += new System.EventHandler(this.textBoxLength_TextChanged);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBoxN.Location = new System.Drawing.Point(141, 49);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(125, 27);
+            this.textBoxN.TabIndex = 6;
+            this.textBoxN.TextChanged += new System.EventHandler(this.textBoxN_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "左分支角度：";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "右分支角度：";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBoxColor);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(312, 441);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "视觉效果";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Location = new System.Drawing.Point(116, 96);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxColor.TabIndex = 1;
+            this.comboBoxColor.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "画笔颜色：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 565);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxLdegree;
+        private System.Windows.Forms.TextBox textBoxRdegree;
+        private System.Windows.Forms.TextBox textBoxLLrate;
+        private System.Windows.Forms.TextBox textBoxRLrate;
+    }
+}
+
---
+++ Homework7/7.0/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _7._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxColor.Items.Add("Red");
+            comboBoxColor.Items.Add("Blue");
+            comboBoxColor.Items.Add("Yellow");
+        }
+        
+        //以下为课本中例5-31源代码
+        private Graphics graphics;
+        private double th1 = 30 * Math.PI / 180;
+        private double th2 = 20 * Math.PI / 180;
+        private double per1 = 0.6;
+        private double per2 = 0.7;
+        private int n = 10;
+        private int length = 100;
+        Pen pen = Pens.Blue;
+
+
+        private void drawCayleyTree(int n, double x0, double y0, double length, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + length * Math.Cos(th);
+            double y1 = y0 + length * Math.Sin(th);
+            
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * length, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * length, th - th2);
+
+        }
+
+        private void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                pen,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        //以下内容结合可以确定的参数进行修改
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = panel1.CreateGraphics();
+            graphics.Clear(Color.White);
+            drawCayleyTree(n, 200, 310, length, -Math.PI / 2);
+        }
+        
+        
+        //视觉效果有关combobox设置
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxColor.Text)
+            {
+                case "Red":
+                    pen = Pens.Red;
+                    break;
+                case "Blue":
+                    pen = Pens.Blue;
+                    break;
+                case "Yellow":
+                    pen = Pens.Yellow;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+        //参数设置有关textbox的设置
+        private void textBoxN_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.Parse(textBoxN.Text) <= 0) { textBoxN.Text = "1"; }
+            else if (Int32.Parse(textBoxN.Text) >= 20) { textBoxN.Text = "20"; }
+            else { n = Int32.Parse(textBoxN.Text); }
+        }
+
+        private void textBoxLength_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.Parse(textBoxLength.Text) <= 0) { textBoxLength.Text = "1"; }
+            else if(Int32.Parse(textBoxLength.Text) >= 100) { textBoxLength.Text = "100"; }
+            else { length = Int32.Parse(textBoxLength.Text); }
+        }
+
+        private void textBoxRLrate_TextChanged(object sender, EventArgs e)
+        {
+            if (double.Parse(textBoxRLrate.Text) < 0) { textBoxRLrate.Text = "0"; }
+            else if (double.Parse(textBoxRLrate.Text) > 1) { textBoxRLrate.Text = "1"; }
+            else { per2 = double.Parse(textBoxRLrate.Text); }
+
+        }
+
+        private void textBoxLLrate_TextChanged(object sender, EventArgs e)
+        {
+            if (double.Parse(textBoxLLrate.Text) < 0) { textBoxLLrate.Text = "0"; }
+            else if (double.Parse(textBoxLLrate.Text) > 1) { textBoxLLrate.Text = "1"; }
+            else { per1 = double.Parse(textBoxLLrate.Text); }
+        }
+
+        private void textBoxRdegree_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.Parse(textBoxRdegree.Text) < 0) { textBoxRdegree.Text = "0"; }
+            else if (Int32.Parse(textBoxRdegree.Text) >= 100) { textBoxRdegree.Text = "100"; }
+            else { th2 = Int32.Parse(textBoxRdegree.Text) * Math.PI / 180; }
+
+        }
+
+        private void textBoxLdegree_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.Parse(textBoxLdegree.Text) < 0) { textBoxLdegree.Text = "0"; }
+            else if (Int32.Parse(textBoxLdegree.Text) >= 100) { textBoxLdegree.Text = "100"; }
+            else { th1 = Int32.Parse(textBoxLdegree.Text) * Math.PI / 180; }
+        }
+    }
+}
