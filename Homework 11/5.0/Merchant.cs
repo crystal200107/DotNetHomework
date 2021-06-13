0785dfb3bd62600cc4b47c21b6855ad27b04100a
crystal200107 <79796600+crystal200107@users.noreply.github.com>
2021-06-14 01:21:44 +0800

    Add files via upload

---
+++ Homework 11/5.0/Merchant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._0
+{
+    public class Merchant
+    {
+        public string StoreName { set; get; }
+        public int StoreId { get; set; }
+        public int ContactNum { get; set; }
+
+        public Merchant(string storeName,int storeID,int contactNum)
+        {
+            this.StoreName = storeName;
+            this.StoreId = storeID;
+            this.ContactNum = contactNum;
+        }
+
+        public override string ToString()
+        {
+            return "商家名称：" + StoreName + " 商家店铺号：" + StoreId + " 售后服务电话：" + ContactNum + "\n";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Merchant merchant &&
+                   StoreName == merchant.StoreName &&
+                   StoreId == merchant.StoreId &&
+                   ContactNum == merchant.ContactNum;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StoreName, StoreId, ContactNum);
+        }
+    }
+}
